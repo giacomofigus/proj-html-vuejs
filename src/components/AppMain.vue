6a8da2bed748dfe596ff8059f72bc23d82fee166
MyUsername <figusgiacomo04@gmail.com>
2024-02-19 13:50:13 +0100

    feat: jumbotron

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -5,10 +5,55 @@
 </script>
 
 <template>
-    <h5>AppMain</h5>
-  
+  <main>
+    <div id="jumboTron">
+      <div id="leftJumbo" class="">
+        <h1>Udemy Affiliate Sales</h1>
+        <p class="fs-5">
+          Monetize your audience and attract new customers with <br>Udemy!
+        </p>
+      </div>
+
+      <div class="position-relative">
+        <img src="../../public/img/slide-1.jpg" alt="" class="">
+
+        <div class="triangle-up"></div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <style lang="scss">
 
+  main{
+    #jumboTron{
+      background-color: #FF4450;
+      margin-top: 30px;
+      color: white;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      overflow: hidden;
+      
+      #leftJumbo{
+        margin-left: 200px;
+
+        h1{
+          font-weight: bolder;
+        }
+      }
+
+      .triangle-up{
+        width: 0;
+	      height: 0;
+	      border-left: 200px solid transparent;
+	      border-right: 200px solid transparent;
+	      border-bottom: 400px solid #FF4450;
+        position: absolute;
+        z-index: 1;
+        top: 0;
+        left: -200px;
+      }
+    }
+  }
 </style>
